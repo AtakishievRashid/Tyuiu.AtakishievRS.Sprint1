0c8bd2b0ab01a028f1eb1e0a05e848a880e47aca
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-09-30 03:10:35 +0500

    Add project files.

---
+++ Tyuiu.AtakishievRS.Sprint1.Task0.V26/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.AtakishievRS.Sprint1.Task0.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Атакишиев Р.С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #26                                                             *");
+        Console.WriteLine("* Выполнил: Атакишиев Р. С. | РППб-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение 4*5/2-18/2/3            *");
+        Console.WriteLine("* и печатает результат на экран.                                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 4*5/2-18/2/3                                                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate());
+
+        Console.ReadLine();
+    }
+}
