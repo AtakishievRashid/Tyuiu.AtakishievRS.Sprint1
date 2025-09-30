9b9c41cfd04f274afb8fcdb21827c784517c60c9
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-09-30 17:34:07 +0500

    Task1

---
+++ Tyuiu.AtakishievRS.Sprint1.Task1.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AtakishievRS.Sprint1.Task1.V3.Lib;
+namespace Tyuiu.AtakishievRS.Sprint1.Task1.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 1.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(3.2, res);
+        }
+    }
+}
