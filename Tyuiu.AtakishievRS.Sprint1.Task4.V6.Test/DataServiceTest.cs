9f2b6a753e77e129114091dffdddb64fd4b6f5f8
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-10-01 08:32:51 +0500

    Task4

---
+++ Tyuiu.AtakishievRS.Sprint1.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AtakishievRS.Sprint1.Task4.V6.Lib;
+namespace Tyuiu.AtakishievRS.Sprint1.Task4.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double z = 0.5;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(z, res);
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint1.Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.AtakishievRS.Sprint1.Task4.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Атакишиев Р.С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Class Math                                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Атакишиев Р. С. | РППб-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        int x, y;
+        Console.WriteLine("Введите значенение X:");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значенение Y:");
+        y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("| y ^ 2 - x | / x * y = " + ds.Calculate(x, y));
+        Console.ReadKey();
+    }
+}
