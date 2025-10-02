d90692e90615509edc8ed6ba40d23d2117b2588f
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-10-02 15:10:39 +0500

    Task7

---
+++ Tyuiu.AtakishievRS.Sprint1.Task7.V14.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AtakishievRS.Sprint1.Task7.V14.Lib;
+namespace Tyuiu.AtakishievRS.Sprint1.Task7.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 0;
+            double z = 1.626;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(z, res);
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint1.Task7.V14/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.AtakishievRS.Sprint1.Task7.V14.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Атакишиев Р.С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнил: Атакишиев Р. С. | РППб-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+        Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+        Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+        Console.WriteLine("*               2                                                         *");
+        Console.WriteLine("*      -x  5*(x)         2                                                *");
+        Console.WriteLine("* z = 2  + ------ - cos(x) + sin(2*x*y)                                   *");
+        Console.WriteLine("*               3                                                         *");
+        Console.WriteLine("*          3*(x)                                                          *");
+
+        Console.WriteLine("*                                                                         *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        double x, y;
+
+        Console.WriteLine("Введите число X:");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число Y:");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x, y));
+
+        Console.ReadKey();
+
+    }
+}
